32d642fbce2e09d1a78b21c5dbef8e1e7e3e394f
Niro Fang <nirofang@outlook.com>
2017-08-02 23:46:52 +0800

    Fix web 2

--- VhiecleWebNJS/routes/home.js
+++ VhiecleWebNJS/routes/home.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var CustomerDAO=require('../DAO/CustomerDAO');
 
+require('date-utils');
 
 
 /* GET home page. */
@@ -32,9 +33,54 @@ router.get('/addUser',function(req,res,next){
 
 });
 
-router.get('/ChangeValidDate',function(req,res,next){
-  res.send('ok');
-  console.log(req.query.operation);
+router.post('/ChangeValidDate',function(req,res,next){
+	var extendSel = parseInt(req.body.extendSel);
+	var valid;
+	switch(extendSel)
+	{
+		case -1:
+			valid = Date.now();
+			break;
+	    case 0:
+			valid = Date.parse("2222-2-2");
+			break;
+	    case 1:
+			valid = (new Date(Date.parse(req.body.valid))).addMonths(1);
+			break;
+	    case 3:
+			valid = (new Date(Date.parse(req.body.valid))).addMonths(3);
+			break;
+	    case 6:
+			valid =  (new Date(Date.parse(req.body.valid))).addMonths(6);
+			break;
+	    case 12:
+			valid =  (new Date(Date.parse(req.body.valid))).addYears(1);
+			break;
+	    case 24:
+			valid =  (new Date(Date.parse(req.body.valid))).addYears(2);
+			break;	
+		default:
+		break;
+	}
+	CustomerDAO.update({
+		MachineCode:req.body.id
+	},{ValidDate:valid},
+	function(err,result){
+		//console.log(err);
+		console.log(result);
+		if(err)
+		{
+			res.send({ err:err});
+		}
+		else
+		{
+			res.send('ok');
+			//res.send({ result:result});
+			
+		}
+		//res.send({ title: 'Express' , a:err, b:result});
+	});	
+
 });
 
 module.exports = router;
---
+++ VhiecleWebNJS/routes/test.js
@@ -0,0 +1,5 @@
+console.log(Date.parse("2017-08-02"));
+console.log(Date.parse("2017-08-01"));
+
+console.log(Date.parse("2017-08-02") - Date.parse("2017-08-01"));
+console.log(new Date(1501632000000));
